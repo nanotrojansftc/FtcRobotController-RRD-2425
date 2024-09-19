975901f84ac4ebc43e3a3985678bcac392c5a416
Daniel Jagger <danieljagger628@gmail.com>
2024-09-18 19:39:17 -0700

    Motors addad to red auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/redAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/redAuto.java
@@ -1,22 +1,51 @@
 package org.firstinspires.ftc.teamcode.Auto;
 
-import androidx.annotation.NonNull;
-
 //RR-specific imports
-import com.acmerobotics.dashboard.config.Config;
-import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
-import com.acmerobotics.roadrunner.Action;
-import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.SequentialAction;
-import com.acmerobotics.roadrunner.Vector2d;
-import com.acmerobotics.roadrunner.ftc.Actions;
 
 // Non-RR imports
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.acmerobotics.roadrunner.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
-// Dont edit!!!!!!!!!!!!!!!!!!!!!!
-public class redAuto {
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+// Don't edit!!!!!!!!!!!!!!!!!!!!!!
+public abstract class redAuto extends LinearOpMode {
+    public static class DcMotorA {
+        private DcMotorEx DcMotorA;
+
+        public DcMotorA(HardwareMap hardwareMap) {
+            DcMotorA = hardwareMap.get(DcMotorEx.class, "Drive1");
+        }
+    }
+
+    public class DcMotorB {
+        private DcMotorEx DcMotorB;
+
+        public DcMotorB(HardwareMap hardwareMap) {
+            DcMotorB = hardwareMap.get(DcMotorEx.class, "Drive2");
+        }
+    }
+
+    public class DcMotorC {
+        private DcMotorEx DcMotorC;
+
+        public DcMotorC(HardwareMap hardwareMap) {
+            DcMotorC = hardwareMap.get(DcMotorEx.class, "Drive3");
+        }
+    }
+
+    public class DcMotorD {
+        private DcMotorEx DcMotorD;
+
+        public DcMotorD(HardwareMap hardwareMap) {
+            DcMotorD = hardwareMap.get(DcMotorEx.class, "Drive4");
+        }
+    }
+
+    public void runOpMode() {
+        // instantiate your MecanumDrive at a particular pose.
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(11.8, 61.7, Math.toRadians(90)));
+    }
 }
