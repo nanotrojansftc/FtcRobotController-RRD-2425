3e3945dbaa61ee0229161ed918e4e5da2a7e6f13
Daniel Jagger <danieljagger628@gmail.com>
2024-11-11 16:20:46 -0800

    Extenders are now extending w/o encoder output

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/opMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/opMode.java
@@ -3,13 +3,13 @@ package org.firstinspires.ftc.teamcode.Teleop;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorControllerEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name="opMode", group="Linear OpMode")
 public class opMode extends LinearOpMode {
-    static final int LEFT_EXTENDER_ENDSTOP = 1;
-    static final int RIGHT_EXTENDER_ENDSTOP = 1;
+    //static final int LEFT_EXTENDER_ENDSTOP = 1000;
+    //static final int RIGHT_EXTENDER_ENDSTOP = 1000;
     //Jesus recommended it and said it would work
    static final float EXTENDER_SCALING = 1.0f/3;
     public void runOpMode() {
@@ -19,23 +19,32 @@ public class opMode extends LinearOpMode {
         DcMotor rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         DcMotor leftExtender = hardwareMap.get(DcMotor.class, "leftExtender");
         DcMotor rightExtender = hardwareMap.get(DcMotor.class, "rightExtender");
+        Servo wristServo = hardwareMap.get(Servo.class, "wristServo");
+        leftExtender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        rightExtender.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
 
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
 
 
-
         waitForStart();
-        while (opModeIsActive()){
+        while (opModeIsActive()) {
+            telemetry.addData("leftFront Pos:", leftFront.getCurrentPosition());
+            telemetry.addData("leftBack Pos:", leftBack.getCurrentPosition());
+            telemetry.addData("rightBack Pos:", rightBack.getCurrentPosition());
+            telemetry.addData("rightFront  Pos:", rightFront.getCurrentPosition());
+            telemetry.addData("leftExtender Pos", leftExtender.getCurrentPosition());
+            telemetry.addData("rightExtender Pos", rightExtender.getCurrentPosition());
 
-            if (gamepad1.left_stick_y != 0){
+            if (gamepad1.left_stick_y != 0) {
                 leftFront.setPower(gamepad1.left_stick_y);
                 leftBack.setPower(gamepad1.left_stick_y);
                 rightBack.setPower(gamepad1.left_stick_y);
                 rightFront.setPower(gamepad1.left_stick_y);
             }
 
-            else if (gamepad1.right_stick_x != 0){
+            if (gamepad1.right_stick_x != 0) {
                 leftFront.setPower(-gamepad1.right_stick_x);
                 leftBack.setPower(-gamepad1.right_stick_x);
                 rightBack.setPower(gamepad1.right_stick_x);
@@ -43,48 +52,52 @@ public class opMode extends LinearOpMode {
 
             }
 
-            else if (gamepad1.left_stick_y == 0) {
+            if (gamepad1.left_stick_y == 0) {
                 leftFront.setPower(0);
                 leftBack.setPower(0);
                 rightBack.setPower(0);
                 rightFront.setPower(0);
             }
-           if (gamepad1.dpad_left){
+            if (gamepad1.dpad_left) {
                 leftFront.setPower(1);
                 leftBack.setPower(-1);
                 rightBack.setPower(1);
                 rightFront.setPower(-1);
             }
-           if (gamepad1.dpad_right){
+            if (gamepad1.dpad_right) {
                 leftFront.setPower(-1);
                 leftBack.setPower(1);
                 rightBack.setPower(-1);
                 rightFront.setPower(1);
             }
-           if(gamepad2.left_stick_y != 0){
-               if(leftExtender.getCurrentPosition() > LEFT_EXTENDER_ENDSTOP){
-                   leftExtender.setPower(0);
-               }
-               else{
-                   leftExtender.setPower(gamepad2.left_stick_y * EXTENDER_SCALING);
-               }
-
-               if(rightExtender.getCurrentPosition() > RIGHT_EXTENDER_ENDSTOP){
-                   rightExtender.setPower(0);
-               }
-               else{
-                   rightExtender.setPower(gamepad2.left_stick_y * EXTENDER_SCALING);
-               }
 
 
-           }
+            if (gamepad2.left_stick_y != 0) {
+                leftExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                rightExtender.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+                leftExtender.setPower(-gamepad2.left_stick_y * EXTENDER_SCALING);
+                rightExtender.setPower(gamepad2.left_stick_y * EXTENDER_SCALING);
+            }
 
-            telemetry.addData("leftFront Pos:", leftFront.getCurrentPosition());
-            telemetry.addData("leftBack Pos:", leftBack.getCurrentPosition());
-            telemetry.addData("rightBack Pos:", rightBack.getCurrentPosition());
-            telemetry.addData("rightFront  Pos:", rightFront.getCurrentPosition());
+            else {
+                leftExtender.setPower(0);
+                rightExtender.setPower(0);
+            }
+        }
+
+        if(gamepad2.a){
+            wristServo.setPosition(1);
+        }
+        if(gamepad2.b){
+            wristServo.setPosition(0);
+        }
+
+        telemetry.addData("Left stick Y: ", gamepad2.left_stick_y);
+        telemetry.addData("Righto stick Y: ", gamepad2.right_stick_y);
+
             telemetry.update();
         }
     }
-}
+
+
