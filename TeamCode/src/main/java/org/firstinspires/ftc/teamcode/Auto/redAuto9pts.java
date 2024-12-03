229e3ce38c94cc3c205185c684867978fd97ebcb
Daniel Jagger <danieljagger628@gmail.com>
2024-12-02 16:54:45 -0800

    Two autos, and both work 😄 (Now with red autos). One parks (3 points) and one scores 2 pieces and parks (9 points).

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/redAuto9pts.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Robot.Drive;
+
+@Autonomous(name="redAuto (9Pts scoring)", group="Autonomous")
+public  class redAuto9pts extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        waitForStart();
+        Drive drive = new Drive(hardwareMap);
+            if(opModeIsActive() && !isStopRequested()){
+            drive.setRotateDegrees(45);
+            drive.driveTiles(1.3f);
+            drive.setRotateDegrees(-45);
+            drive.driveTiles(1.5f);
+            drive.setRotateDegrees(180);
+            drive.driveTiles(2.5f);
+            drive.driveTiles(-2.5f);
+            drive.strafeTiles(1);
+            drive.driveTiles(2.5f);
+            }
+            while (opModeIsActive()){
+            telemetry.addLine(drive.gatherMotorPos());
+            telemetry.update();
+            }
+    }
+}
